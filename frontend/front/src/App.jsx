99bdb9decdc39d05a010fd20233aac6e6e5b9c3e
gabriel araújo <bsdgalatico@gmail.com>
2025-04-29 11:25:50 -0300

    atualizar

---
+++ frontend/front/src/App.jsx
@@ -0,0 +1,30 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
+
+function App() {
+
+const[btnCadastrar] =useState(true);
+const[produtos, setProdutos] =useState([]);
+
+
+  //UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+
+return (    
+      <div>
+        <p>{JSON.stringify(produtos)}</p>
+        <Formulario botao={btnCadastrar}/>
+        <Tabela/>
+      </div>
+  )
+}
+
+export default App;
